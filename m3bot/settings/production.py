c53f9943c716c0dcc8c5c5e37b10f43c349dedfb
ismael <ismael.masharo@gmail.com>
2020-04-26 18:47:02 -0500

    adding gunicorn

--- m3bot/settings/production.py
+++ m3bot/settings/production.py
@@ -2,3 +2,14 @@ from .base import *
 
 DEBUG = False
 ALLOWED_HOSTS = ["musixbot.herokuapp.com"]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'm3bot',
+        'USER': 'ismael',
+        'PASSWORD': 'ismael',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
