33809f401694b91a32c248395b8e8a5168459bd2
ismael <ismael.masharo@gmail.com>
2020-04-26 23:46:28 -0500

    fix stat parse

--- bot/statistics.py
+++ bot/statistics.py
@@ -3,10 +3,10 @@ from django.db.models import Count, Avg, F
 def stats_text():
     from .models import BotUser, MxmTrack, UserSession
 
-    msg = "*Estadisticas* \n"
+    msg = "*Estadisticas*\n"
 
     # Number of users
-    msg += "Número de usuarios: " + str(BotUser.objects.all().count()) + "\n"
+    msg += "Número de usuarios: %s\n" % str(BotUser.objects.all().count())
 
     # Top songs
     track_fav = MxmTrack.objects.annotate(
@@ -17,16 +17,16 @@ def stats_text():
 
     fav_number = min(10, len(track_fav))
     top_songs = track_fav.order_by('num_favorite')[:fav_number]
-    msg += "Top " + str(fav_number) + " canciones favoritas:\n"
+    msg += "Top %s canciones favoritas:\n" % str(fav_number)
 
     for song in top_songs:
-        msg += "- " + str(song) + "\n"
+        msg += "- %s \n" % str(song)
 
     # Average user session
     avg_s = UserSession.objects.aggregate(
         average_session=Avg(F('end_time') - F('start_time'))
     )
     average = str(avg_s['average_session']).split(".")[0]
-    msg += "Tiempo promedio por sesión: %s" % average + "\n"
+    msg += "Tiempo promedio por sesión: %s \n" % average
 
     return msg
